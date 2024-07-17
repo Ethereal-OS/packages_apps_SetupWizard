395e4eb1ff68af5490a1a7d7ad316fe3ac4c01ec
Oliver Scott <olivercscott@gmail.com>
2024-07-16 21:25:29 -0400

    Check bluetooth status when relevant
    Change-Id: Ie67a28cb89bf68ef0d57f36ee2d875cfc44e29cc
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/org/lineageos/setupwizard/util/SetupWizardUtils.java
+++ src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -47,7 +47,6 @@ import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import org.lineageos.setupwizard.BaseSetupWizardActivity;
-import org.lineageos.setupwizard.BluetoothSetupActivity;
 import org.lineageos.setupwizard.SetupWizardApp;
 
 import java.io.File;
@@ -224,9 +223,7 @@ public class SetupWizardUtils {
     }
 
     public static void disableComponentsForMissingFeatures(Context context) {
-        if (!hasLeanback(context) || isBluetoothDisabled()) {
-            disableComponent(context, BluetoothSetupActivity.class);
-        }
+
     }
 
     /**
